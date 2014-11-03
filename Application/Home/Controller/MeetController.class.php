bea84026aadc0ac457720cd5f1aed52b67b88b1f
zhlhuang <364626853@qq.com>
2014-11-03 18:32:46 +0800

    增加对meet表操作

--- Application/Home/Controller/MeetController.class.php
+++ Application/Home/Controller/MeetController.class.php
@@ -8,18 +8,151 @@ class MeetController extends Controller {
 	public function index(){
 		echo "hello";
 	}
+	/*
+	 *
+	* 添加会议
+	*   
+	"title": "袂卓第二次会议",
+    "intro": "这是我们第二次全体例会",
+    "address": "北主楼1608",
+    "createmember": "2",   //可以从客户端传过来
+    "weight": "20",
+    "starttime": "Y-m-d H:i:s", eg 2014-09-25 21:30:25
+    "endtime": "Y-m-d H:i:s"
+	*/
+	
+	public  function addmeet(){
+		$data=I("post.");
+		$member=session("member");
+		if($member){
+			if(!$data){ //post 数据为空  真返回错误提示
+				echo "post error";
+				exit;
+			}
+			$data["createmember"]=$member["id"];//创建会议人
+			$meetmodel=D("meet");
+			$res=$meetmodel->addmeet($data);
+			
+			if($res){
+				echo "success"; //添加会议成功
+			}else{
+				echo "error"; //添加会议失败
+			}
+			
+			print_r($data);
+		}else{
+			echo "login error"; //用户没有登录，添加失败
+		}
+	//	echo date("Y/m/d H:i:s",$data["endtime"]);
+	}
+	
+	/*
+	 *
+	* 添加会议
+	*
+	"id":2
+	"title": "袂卓第二次会议",
+	"intro": "这是我们第二次全体例会",
+	"address": "北主楼1608",
+	"createmember": "2",   //可以从客户端传过来
+	"weight": "20",
+    "starttime": "Y-m-d H:i:s", eg 2014-09-25 21:30:25
+    "endtime": "Y-m-d H:i:s"
+	* 上面是我们需要修改的数据
+	*/
+	public function updatemeet(){
+		$data=I("post.");
+		$meetmodel=D("meet");
+		if($data){
+			$res=$meetmodel->updatemeet($data);
+			if($res){
+				echo "success";
+			}else{
+				echo "data no update";
+			}
+		}else{
+			echo "post error";
+		}
+		
+		
+	}
+	
+	
+	/*
+	 *
+	* 删除相应的会议
+	* meetid 会议的id
+	*/
+	
+	public function deletemeet(){
+		$meetid=I("post.meetid");
+		$meetmodel=D("meet");
+		if($meetid){
+		 $res=$meetmodel->where("id=".$meetid)->delete();
+		 if($res){
+		 	echo "success";
+		 }else{
+		 	echo "error";
+		 }
+		}else{
+			echo "post error";
+		}
+		
+	}
 	
 	/*
 	 *
 	* 获取相应的会议
-	*
+	*meetid 指定查找id  为空是获得全部会议
+	*field 是json 格式 {"0":"title","1":"address"}
 	*/
 	
 	public function getmeet(){
-		$meetid=I("get.meetid");//获取相应的id
+		$meetid=I("post.meetid");//获取相应的id
+		$field=I("post.field",'',''); //拿到想要获得的字段  用json的格式传送
 		
+		if($field){//如果指定了想要的字段，则要通过json的格式输入
+			$field=json_decode($field,1);
+		}
+		
+		$meetmodel=D("meet");
+		
+		
+		
+		if($meetid==NULL){
+			$res=$meetmodel->select();
+		}else{
+			$res=$meetmodel->field($field)->where("id=".$meetid)->find();
+		}
+		$this->AjaxReturn($res);
 	}
 	
+	/*
+	 *
+	* 会议添加成员
+	*meetid 指定查找id  为空是获得全部会议
+	*field 是json 格式 {"0":"title","1":"address"}
+	*/
+	
+	public function addjoin(){
+		$data=I("post.");
+		$joinmodel=D("joinmeet");
+
+		if($data){
+			$data["stime"]=time();
+			//print_r($data);
+		
+			$res=$joinmodel->add($data);
+			if($res){
+				echo "success";
+			}else{
+				echo "error";
+			}
+			
+		}else{
+			echo "post error";
+		}
+	}
 	
 	
 }
--- Application/Home/Model/MeetModel.class.php
+++ Application/Home/Model/MeetModel.class.php
@@ -2,10 +2,20 @@
 namespace Home\Model;
 use Think\Model;
 class MeetModel extends Model{
-	/*
-	 *
-	* 这里是会员的登录界面
-	*
-	*/
+	public function addmeet($data){
+			
+		$data["starttime"]=strtotime($data["starttime"]);
+		$data["endtime"]=strtotime($data["endtime"]);	
+		
+		return $this->add($data);
+	}
 	
+	public function updatemeet($data){
+		
+
+		$data["starttime"]=strtotime($data["starttime"]);
+		$data["endtime"]=strtotime($data["endtime"]);
+		
+		return $this->save($data);
+	}
 }
