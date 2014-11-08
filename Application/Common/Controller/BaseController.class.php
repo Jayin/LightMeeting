18fe2f84d1f7332d517d1326b64332441cb41d7a
Jayin Ton <tonjayin@gmail.com>
2014-11-08 15:17:49 +0800

    * add：_empty() * update:comments

--- Application/Common/Controller/BaseController.class.php
+++ Application/Common/Controller/BaseController.class.php
@@ -5,6 +5,17 @@ namespace Common\Controller;
 use Think\Controller;
 
 class BaseController extends Controller {
+	/**
+	 * 对应控制器下没有方法就会执行该方法
+	 */
+	protected function _empty(){
+		$this->ajaxReturn(qc_json_error("No interface for this."));
+	}
+	/**
+	 * 需要Post的字段
+	 * @param array $require_data
+	 * @return \Common\Controller\BaseController
+	 */
 	protected function reqPost(array $require_data = null) {
 		if (! IS_POST) {
 			$this->ajaxReturn(qc_json_error_request());
@@ -20,7 +31,10 @@ class BaseController extends Controller {
 		return $this;
 	}
 	
-	
+	/**
+	 * 获得登陆信息
+	 * @return \Common\Controller\BaseController
+	 */
 	protected function getlogin(){
 		$member=session("member");
 		if($member==NULL){
