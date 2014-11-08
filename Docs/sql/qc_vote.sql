c12ef2cfaf771273f44ac20da8c4dd086618606c
Jayin Ton <tonjayin@gmail.com>
2014-11-08 13:52:24 +0800

    * 更新:会议的字段 * 新增:3个投票表

---
+++ Docs/sql/qc_vote.sql
@@ -0,0 +1,14 @@
+--
+-- 表的结构 `qc_vote`
+--
+DROP TABLE IF EXISTS `qc_vote`;
+CREATE TABLE `qc_vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '投票id',
+  `meetid` int(11) NOT NULL COMMENT '所属会议id',
+  `title` varchar(64) NOT NULL COMMENT '投票主题',
+  `intro` varchar(1024) NOT NULL COMMENT '投票简介',
+  `ctime` int(10) NOT NULL COMMENT '起始时间',
+  `etime` int(10) NOT NULL COMMENT '终止时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='投票表' AUTO_INCREMENT=1 ;
+
