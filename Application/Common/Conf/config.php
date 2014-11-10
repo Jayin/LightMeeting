4cf87bc2f2d591dbbecaba4959b74761becbceb5
zjien <694396727@qq.com>
2014-11-10 21:03:29 +0800

    修改phtml后缀

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array (
 		'LOG_TYPE' => 'File', // 日志记录类型 默认为文件方式
 		'URL_PARAMS_BIND' => true,
 		'URL_PARAMS_BIND_TYPE' => 1, // 设置参数绑定按照变量顺序绑定
-		'TMPL_TEMPLATE_SUFFIX' => '.phtml',
+		'TMPL_TEMPLATE_SUFFIX' => '.html',
 		//'SESSION_TYPE' => 'Db', // 用数据库的形式存储session
 		'URL_HTML_SUFFIX' => 'html|phtml', // 伪静态后缀名设置
 		'LOAD_EXT_FILE' => 'functions',
