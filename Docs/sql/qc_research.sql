012717f29f72ba1f1af3045dc25c3688dbf9106f
Jayin Ton <tonjayin@gmail.com>
2014-11-15 17:12:37 +0800

    修复建表错误 原因： OS X下的MAMP的建表wamp有区别

--- Docs/sql/qc_research.sql
+++ Docs/sql/qc_research.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS `qc_research`;
 CREATE TABLE `qc_research` (
-`id` int(11) NOT NULL COMMENT '调查表id',
+`id` int(11) NOT NULL AUTO_INCREMENT COMMENT '调查表id',
   `meetid` int(11) NOT NULL COMMENT '所属会议id',
   `title` varchar(128) NOT NULL COMMENT '调查主题',
   `intro` text NOT NULL COMMENT '调查简介',
   `stime` int(11) NOT NULL COMMENT '起始时间',
-  `etime` int(11) NOT NULL COMMENT '结束时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='调查表';
+  `etime` int(11) NOT NULL COMMENT '结束时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='调查表' AUTO_INCREMENT=1 ;
--- Docs/sql/qc_research_question.sql
+++ Docs/sql/qc_research_question.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS `qc_research_question`;
 CREATE TABLE `qc_research_question` (
-`id` int(11) NOT NULL COMMENT '回答id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '回答id',
   `researchid` int(11) NOT NULL COMMENT '问题id',
   `title` varchar(128) NOT NULL COMMENT '回答选项内容',
-  `options` text NOT NULL COMMENT '发布人'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='调查问题表';
+  `options` text NOT NULL COMMENT '发布人',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='调查问题表' AUTO_INCREMENT=1;
