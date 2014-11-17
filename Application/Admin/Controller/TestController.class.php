d90f83e031f84749e1341b40cdb8822c995f8203
Jayin Ton <tonjayin@gmail.com>
2014-11-17 17:04:26 +0800

    add qc.js

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -10,4 +10,19 @@ class TestController extends Controller {
     public function index(){
         $this->display();
     }
+    
+    public function get1(){
+    	$this->ajaxReturn(qc_json_success());
+    }
+    
+    public function  get2($name=1,$id=2){
+    	$this->ajaxReturn(qc_json_success(array('name'=>$name,'id'=>$id)));
+    }
+    
+    public function post(){
+    	$this->ajaxReturn(qc_json_success(I('post.')));
+    }
+    public function post1(){
+    	$this->ajaxReturn(qc_json_error(I('post.')));
+    }
 }
---
+++ Public/js/qc.js
@@ -0,0 +1,72 @@
+/**
+ * 封装GET POST的请求
+ * @author Jayin Ton
+ */
+if ("undefined" == typeof $) throw new Error("QC Client's JavaScript requires jQuery"); 
+
+var qc = {};
+qc.base_url = "/LightMeeting";
+qc.debug = false;
+
+qc.log = function(msg){
+    if(this.debug){
+        console.log(msg);
+    }
+}
+//  GET
+qc.get =  function (url,params,success_handler,error_hander,faild_hander){
+    // $.get(url,params)
+    if(params && typeof params === 'object'){
+         for (var key in params){
+            url += '/' + key + '/' + params[key];
+        }
+    }
+    url = qc.base_url + url;
+
+    qc.log("GET url : " );
+    qc.log(url);
+
+    $.get(url,function(json){
+        qc.log("Return : " );
+        qc.log(json);
+        //success
+        if(json.code){
+            if(success_handler){
+                success_handler(json.response);
+            }
+        }else{
+            //error
+            if(error_hander){
+                error_hander(json.msg);
+            }
+        }
+    },'json').fail(faild_hander);
+}
+// POST
+qc.post = function(url,data,success_handler,error_hander,faild_hander){
+    url = qc.base_url + url;
+
+    qc.log("POST url : " );
+    qc.log(url);
+    qc.log("Data : " );
+    qc.log(data);
+
+    $.post(url,data,function(json){
+        qc.log("Return : " );
+        qc.log(json);
+        if(json.code){
+            if(success_handler){
+                success_handler(json.response);
+            }
+        }else{
+            if(error_hander){
+                error_hander(json.msg);
+            }
+        }
+    },'json');
+}
+
+
+
+
+
