280ce566ff87c1ce22e507e693a864c9902a2244
zhlhuang <364626853@qq.com>
2014-11-17 16:14:47 +0800

    加入了flat模板，成功实现登录界面。

---
+++ Application/Admin/Controller/MemberController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Admin\Controller;
+
+use Common\Controller\BaseController;
+
+
+class MemberController extends BaseController {
+    /**
+     * 会员登录
+     * 
+     *   */
+    public function login(){
+        $this->display();
+    }
+}
