7f08e856574147da692d9ac1530f70f267c840dd
Jayin Ton <tonjayin@gmail.com>
2014-11-18 22:35:27 +0800

    add jpush

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,6 +8,9 @@ return array (
 		'URL_HTML_SUFFIX' => 'html|phtml', // 伪静态后缀名设置
 		'LOAD_EXT_FILE' => 'functions',
 		'URL_CASE_INSENSITIVE' =>true, //不区分URL大小写
+        //JPush 
+        'JPUSH_APP_KEY' => 'aba9dfd2ab6d4a7e061b95b5',
+        'JPUSH_MASTER_SECRET' => '3c7e5d499b69f81675384a66'
 )
 ;
 
--- Application/Home/Controller/PushMessageController.class.php
+++ Application/Home/Controller/PushMessageController.class.php
@@ -3,6 +3,11 @@
 namespace Home\Controller;
 
 use Common\Controller\BaseController;
+use JPush\Model as M;
+use JPush\JPushClient;
+
+use JPush\Exception\APIConnectionException;
+use JPush\Exception\APIRequestException;
 /** 
  * 推送消息接口
  * @author zjien
@@ -33,5 +38,24 @@ class PushMessageController extends BaseController{
     	 
     	$res = D('PushMessage')->lists($page,$limit);
     	$this->ajaxReturn(qc_json_success($res));
+    }
+    //simple send message
+    public function sendMessage($msg,$extras = null){
+        vendor('JPushSDK.autoload');
+
+        $client = new JPushClient(C('JPUSH_APP_KEY'), C('JPUSH_MASTER_SECRET'));
+        try {
+            $result = $client->push()
+            ->setPlatform(M\all)
+            ->setAudience(M\all)
+            ->setMessage(M\message('msg content', null, null, array('key'=>'value')))
+            ->send();
+            $this->ajaxReturn(qc_json_success('发送成功'));
+        }catch (APIRequestException $e) {
+            $this->ajaxReturn(qc_json_error($e->message,$e->code));
+        } catch (APIConnectionException $e) {
+            $this->ajaxReturn(qc_json_error($e->getMessage()));
+        }
+                
     }
 }
