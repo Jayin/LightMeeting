84fd51bae3cc6ee148894a1ff0501349c7a5654c
Jayin Ton <tonjayin@gmail.com>
2014-11-22 14:24:55 +0800

    会议详情生成二维码页面

---
+++ Application/Admin/Controller/ViewController.class.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Admin\Controller;
+
+use Admin\Controller\AdminBaseController;
+/**
+ * 查看
+ * @author Jayin Ton
+ *
+ */
+class ViewController extends AdminBaseController {
+	/**
+	 * 查看一会议的详情
+	 * @param number $id 会议id
+	 */
+	public function meeting($id = 0){
+// 		$this->reqLogin();
+        $Meetmodel=D("Meet");
+       
+        $LoginMember=$this->reqLoginmember(); //获取登录用户
+        $res=$Meetmodel->findmeet($id);  //查找对应meet
+         
+        
+        if(isset($res["code"])){
+            $Meet=$res["response"];
+            
+            $Membermodel=D("Member"); //获取相关联的会议创建人
+            $resMember=$Membermodel->getMemberInfo($Meet["createmember"])["response"];
+            
+            $starttime=$Meet["starttime"];
+            $Meet["starttime"]=date("Y-m-d",$starttime);
+            $Meet["starttime-h"]=date("H",$starttime);
+            $Meet["starttime-i"]=date("i",$starttime);
+            
+            $endtime=$Meet["endtime"];
+            $Meet["endtime"]=date("Y-m-d",$endtime);
+            $Meet["endtime-h"]=date("H",$endtime);
+            $Meet["endtime-i"]=date("i",$endtime);
+ 
+            $this->assign("Meet",$Meet);
+            $this->assign("LoginMemberid",$LoginMember["id"]); //分配登录id
+            $this->assign("Member",$resMember);
+            
+        }else{
+            $this->error("查不到此会议");
+        }
+         $this->display();
+	}
+}
+
