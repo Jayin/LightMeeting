b65a8799f717296745cdf678999472060adac0d8
Jayin Ton <tonjayin@gmail.com>
2014-11-22 14:24:29 +0800

    * rm useless code * add comments

--- Application/Admin/Controller/AdminBaseController.class.php
+++ Application/Admin/Controller/AdminBaseController.class.php
@@ -5,7 +5,9 @@ namespace Admin\Controller;
 use Common\Controller\BaseController;
 
 class AdminBaseController extends BaseController {
-    
+    /**
+     * 需要登录，没有登录则跳转到登录页
+     */
     public function reqLogin(){
         $member=session("member");
 		if($member==NULL){
