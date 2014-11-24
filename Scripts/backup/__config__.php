b2f7f2598d230c22ef21257ef12a799998daecd9
Jayin Ton <tonjayin@gmail.com>
2014-11-24 20:06:52 +0800

    default db should be `qcdatabase`

--- Scripts/backup/__config__.php
+++ Scripts/backup/__config__.php
@@ -5,7 +5,7 @@ if(file_exists('./server_config.php')){
 }
 return array(
 	'DB_HOST' => 'localhost', // 服务器地址
-	'DB_NAME' => 'mz', // 数据库名
+	'DB_NAME' => 'qcdatabase', // 数据库名
 	'DB_USER' => 'root', // 用户名
 	'DB_PWD' => 'root', // 密码
 	'DB_PORT' => '3306', // 端口
