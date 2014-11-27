92f988b81a8ff7e572833744bb61c61426d12dd4
zhlhuang <364626853@qq.com>
2014-11-27 22:55:50 +0800

    增加参加会议人员信息

--- Application/Common/Model/JoinmeetModel.class.php
+++ Application/Common/Model/JoinmeetModel.class.php
@@ -85,7 +85,7 @@ class JoinmeetModel extends BaseModel
     public function getjoinmember($meetid)
     {
         // 通过会议id查找参加会议人员
-        $res = $this->field("m.id,m.username,m.nickname,m.sex,m.company,m.position,j.stime")
+        $res = $this->field("j.stime,j.checkin,j.checkin_time,m.id,m.username,m.nickname,m.sex,m.company,m.position")
             ->table("qc_joinmeet j,qc_member m")
             ->where("m.id=j.memberid and j.meetid=" . $meetid)
             ->select();
