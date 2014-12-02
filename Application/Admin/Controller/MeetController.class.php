90faf4031e61f3d871b85bb23b43ad7bfde8a20d
Jayin Ton <tonjayin@gmail.com>
2014-12-02 20:33:23 +0800

    Fix #21 :关于我们的页面

--- Application/Admin/Controller/MeetController.class.php
+++ Application/Admin/Controller/MeetController.class.php
@@ -10,27 +10,27 @@ use Common\Model\PushMessageModel;
  *
  * @author Jayin Ton
  * @author zhlhuang
- *        
+ *
  */
 class MeetController extends AdminBaseController {
 	public function index() {
 		$this->redirect ( 'member/index' );
 	}
-	
+
 	/**
 	 * 添加会议
 	 */
 	public function createmeet() {
 		$this->display ();
 	}
-	
+
 	/**
 	 * 会议更新
 	 */
 	public function updatemeet($id) {
 		// echo $id;
 		$this->reqLogin ();
-		
+
 		$MeetModel = D ( "Meet" );
 		$res = $MeetModel->findmeet ( $id );
 		$data = $res ["response"];
@@ -39,36 +39,36 @@ class MeetController extends AdminBaseController {
 			$data ["starttime"] = date ( "Y-m-d", $starttime );
 			$data ["starttime-h"] = date ( "H", $starttime );
 			$data ["starttime-i"] = date ( "i", $starttime );
-			
+
 			$endtime = $data ["endtime"];
 			$data ["endtime"] = date ( "Y-m-d", $endtime );
 			$data ["endtime-h"] = date ( "H", $endtime );
 			$data ["endtime-i"] = date ( "i", $endtime );
-			
+
 			$this->assign ( "Meet", $data );
 			$this->assign ( "id", $id );
 		}
-		
+
 		$this->display ();
 	}
-	
+
 	/**
 	 * 会议详情
 	 */
 	public function detail($id = 0) {
 		$this->reqLogin ();
 		$Meetmodel = D ( "Meet" );
-		
+
 		$LoginMember = $this->reqLoginmember (); // 获取登录用户
 		$res = $Meetmodel->findmeet ( $id ); // 查找对应meet
-		
+
 		if (isset ( $res ["code"] )) {
 			$meet = $res ["response"];
-			
+
 			$Membermodel = D ( "Member" ); // 获取相关联的会议创建人
 			$resMember = $Membermodel->getMemberInfo ( $meet ["createmember"] )["response"];
 			$listmembermodel = D ( "Joinmeet" );
-			
+
 			$listmember=$listmembermodel->getjoinmember($id)["response"];//获取参加会议人员
 /* 			echo "<pre>";
 			print_r($listmember);
@@ -81,10 +81,10 @@ class MeetController extends AdminBaseController {
 		} else {
 			$this->error ( "查不到此会议" );
 		}
-		
+
 		$this->display ();
 	}
-	
+
 	/**
 	 * 会议文档
 	 */
@@ -92,21 +92,21 @@ class MeetController extends AdminBaseController {
 		$this->reqLogin ();
 		$Meet ["id"] = $id; // 将会议id分配前端页面显示
 		$this->assign ( "Meet", $Meet );
-		
+
 		$DocModel = D ( "Document" );
 		$LoginMember = $this->reqLoginmember (); // 获取登录用户
-		
+
 		$res = $DocModel->listmeetdoc1 ( $id );
 		if (isset ( $res ["code"] )) {
 			$doc = $res ["response"];
-			
+
 			$this->assign ( "doc", $doc );
 			$this->assign ( "LoginMemberid", $LoginMember ["id"] ); // 分配登录id
 		}
-		
+
 		$this->display ();
 	}
-	
+
 	/**
 	 * 创建会议文档
 	 *
@@ -119,7 +119,7 @@ class MeetController extends AdminBaseController {
 		$this->assign ( "Meet", $Meet );
 		$this->display ();
 	}
-	
+
 	/**
 	 * 文档更新
 	 *
@@ -130,14 +130,14 @@ class MeetController extends AdminBaseController {
 		$this->reqLogin ();
 		$DocModel = D ( "Document" );
 		$res = $DocModel->listdoc ( $id );
-		
+
 		if ($res) {
 			$doc = $res ["response"];
 			$Meet ["id"] = $doc ["meetid"]; // 将会议id分配前端页面显示
 			$this->assign ( "Meet", $Meet );
 			$this->assign ( "doc", $doc );
 		}
-		
+
 		$this->display ();
 	}
 	/**
@@ -162,18 +162,18 @@ class MeetController extends AdminBaseController {
 		$this->reqLogin ();
 		$Meet ["id"] = $id; // 将会议id分配前端页面显示
 		$this->assign ( "Meet", $Meet );
-		
+
 		$DisModel = D ( "Discuss" );
 		$LoginMember = $this->reqLoginmember (); // 获取登录用户
 		$res = $DisModel->lists_m ( $id );
-		
+
 		if ($res) {
-			
+
 			$dis = $res ["response"];
 			$this->assign ( "dis", $dis );
 			$this->assign ( "LoginMemberid", $LoginMember ["id"] ); // 分配登录id
 		}
-		
+
 		$this->display ();
 	}
 	/**
@@ -184,23 +184,23 @@ class MeetController extends AdminBaseController {
 	 */
 	public function updatediscuss($id) {
 		$this->reqLogin ();
-		
+
 		$DisModel = D ( "Discuss" );
 		$res = $DisModel->info ( $id );
 		if (isset ( $res ["code"] )) {
 			$dis = $res ["response"];
-			
+
 			$Meet ["id"] = $dis ["meetid"]; // 将会议id分配前端页面显示
 			$this->assign ( "Meet", $Meet );
-			
+
 			$this->assign ( "dis", $dis );
 		} else {
 			$this->error ( "找不到该记录" );
 		}
-		
+
 		$this->display ();
 	}
-	
+
 	/**
 	 * 创建调查
 	 *
@@ -217,15 +217,15 @@ class MeetController extends AdminBaseController {
 	}
 	/**
 	 * 更新调查(未完成交互功能)
-	 * 
-	 * @param unknown $id        	
-	 * @param unknown $researchid        	
+	 *
+	 * @param unknown $id
+	 * @param unknown $researchid
 	 */
 	public function updateResearch($id, $researchid) {
 		$this->reqLogin ();
 		$Meet ["id"] = $id; // 将会议id分配前端页面显示
 		$this->assign ( "Meet", $Meet );
-		
+
 		$resResearch = D ( 'Research' )->info ( $researchid );
 		if (isset ( $resResearch ['code'] )) {
 			// questions
@@ -262,12 +262,12 @@ class MeetController extends AdminBaseController {
 	 */
 
 	public function createvote($id){
-		
+
 		$this->reqLogin();
 		$Meet ["id"] = $id; // 将会议id分配前端页面显示
 		$this->assign ( "Meet", $Meet );
 
-		
+
 		$this->display();
 	}
 
@@ -326,4 +326,13 @@ class MeetController extends AdminBaseController {
         $this->assign("Messages",$resPushMessage['response']);
         $this->display();
     }
+
+    /**
+     *关于我们
+     */
+    public function about(){
+        $this->reqLogin();
+        $this->display();
+    }
+
 }
